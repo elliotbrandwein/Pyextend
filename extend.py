883b5ce545acdd724c3e82cf1e76d3e5c19b419c
Elliot Brandwein <elliotbrandwein1@gmail.com>
2016-08-12 12:33:16 -0400

    fixed all the problems I found, and removed false support

--- extend.py
+++ extend.py
@@ -8,109 +8,77 @@ if PY_33:
 else:
     from collections import Mapping, MutableSequence
 
-# This function should behave exactly like the jquery extend function,
-# Except for using extend with 1 argument:
-# If that argument is False, this will return a blank dict, not a function
-# If that argument is a dict/list, this will return back that dict/list
-
 
 def extend(*args):
-
+    options = None
+    name  = None
+    src = None
+    copy = None
+    copyIsArray = None
+    clone = None
     if args:
         target = args[0]
     else:
         target = {}
-    i = 1
     args_length = len(args)
+    i = 1
     deep = False
-    options = None
-    # Handle a deep copy situation
-
     if isinstance(target, bool):
         deep = target
-        # Skip the boolean and the target
-        if i < args_length:
+
+        if args_length > 1:
             target = args[i]
         else:
             target = {}
         i += 1
-
-
-# Handle case when target is a string or something (possible in deep copy)
-    if not isinstance(target, Mapping) and not isinstance(target, FunctionType)\
-            and not isinstance(target, MutableSequence):
+    if not isinstance(target, MutableSequence) and not isinstance(target, Mapping) \
+        and not isinstance(target, FunctionType):
         target = {}
-
-    for index in range(i, args_length):
-        # only deal with non-None values
-        if args[index] is not None:
+    for index in range(i,args_length):
+        if index <= args_length:
             options = args[index]
-            if isinstance(options, Mapping):
-                for name in options:
-                    if target and name in target:
-                        src = target[name]
-                    else:
-                        src = None
-                    if options and name in options:
-                        copy = options[name]
-                    else:
-                        copy = None
+        else:
+            options = None
+        if isinstance(options, Mapping):
+            for name in options:
+                if name in target:
+                    src = target[name]
+                if name in options:
+                    copy = options[name]
+                if target == copy:
+                    continue
 
-                    # Prevent never-ending loop
-                    if target == copy:
-                        continue
-
-                    copy_is_list = isinstance(copy, MutableSequence)
-
-                    # Recurse if we're merging dicts or lists
-                    if deep and copy and (isinstance(copy, Mapping)
-                                          or copy_is_list):
-
-                        if copy_is_list:
-                            if src is not None and isinstance(src,
-                                                              MutableSequence):
-                                clone = src
-                            else:
-                                clone = []
+                if deep and copy and (isinstance(copy, Mapping) \
+                                              or isinstance(copy, MutableSequence)):
+                    copyIsArray = isinstance(copy, MutableSequence)
+                    if copyIsArray:
+                        copyIsArray = False
+                        if src and isinstance(src, MutableSequence):
+                            clone = src
                         else:
-                            if src is not None and isinstance(src, Mapping):
-                                clone = src
-                            else:
-                                clone = {}
-
-                        # never move original objects, clone them
-                        if not isinstance(name, Mapping):
-                            target[name] = extend(deep, clone, copy)
-                        else:
-                            for deep_name in name:
-                                name = deep_name
-                            if name in target:
-                                target[name] = extend(deep, clone, copy)
-
-                    # Don't bring in undefined values
-                    elif copy is not None and target:
-                        target[name] = copy
-
-            # handles a list on a deep copy
-            else:
-                target_length = len(target)
-                overshoot = False
-                for i in range(0, len(options)):
-                    element = options[i]
-
-                    if target_length < i:
-                        overshoot = True
-                    if not isinstance(element, Mapping):
-                        target[i] = element
-                    elif not overshoot and not isinstance(target[i], Mapping):
-                        target[i] = element
-                    # this will take care of overlapping dicts in two lists
+                            src = []
                     else:
-                        target[i] = extend(deep, target[i], element)
-                return target
+                        if src and isinstance(src, Mapping):
+                            clone = src
+                        else:
+                            src = {}
+                    target[name] = extend(deep, clone, copy)
+                elif copy is not None:
+                    target[name] = copy
+        else:
+            target_length = len(target)
+            overshoot = False
+            for i in range(0, len(options)):
+                element = options[i]
 
-    # handles the missing last layer on a deep copy,
-    if len(target) == 0 and options:
-        target = options
-
+                if target_length < i:
+                    overshoot = True
+                if not isinstance(element, Mapping):
+                    target[i] = element
+                elif not overshoot and not isinstance(target[i], Mapping):
+                    target[i] = element
+                # this will take care of overlapping dicts in two lists
+                else:
+                    target[i] = extend(deep, target[i], element)
+            return target
     return target
