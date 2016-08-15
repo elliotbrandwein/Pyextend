624a1934f01e506f48cc45e483ed1a8c25839a11
Elliot Brandwein <elliotbrandwein1@gmail.com>
2016-08-15 14:11:25 -0400

    may have overdone it with the unittests

--- tests/test_extend.py
+++ tests/test_extend.py
@@ -178,12 +178,24 @@ class TestExtend(unittest.TestCase):
     def test_merging_5_things(self):
         sample_dict1 = {"dict1": 1}
         sample_dict2 = {"dict2": 2}
+        sample_dict2_copy = sample_dict2
         sample_dict3 = {"dict3": 3}
+        sample_dict3_copy = sample_dict3
         sample_dict4 = {"dict4": 4}
+        sample_dict4_copy = sample_dict4
         sample_dict5 = {"dict5": 5}
-        result = {"dict1": 1, "dict2": 2, "dict3": 3, "dict4": 4, "dict5": 5}
-        self.assertDictEqual(extend(sample_dict1, sample_dict2, sample_dict3,
-                                    sample_dict4, sample_dict5), result)
+        sample_dict5_copy = sample_dict5
+        result = extend(sample_dict1, sample_dict2, sample_dict3, sample_dict4,
+                        sample_dict5)
+        correct_result = {"dict1": 1, "dict2": 2, "dict3": 3, "dict4": 4,
+                          "dict5": 5}
+
+        self.assertDictEqual(sample_dict2, sample_dict2_copy)
+        self.assertDictEqual(sample_dict3, sample_dict3_copy)
+        self.assertDictEqual(sample_dict4, sample_dict4_copy)
+        self.assertDictEqual(sample_dict5, sample_dict5_copy)
+        self.assertDictEqual(sample_dict1, result)
+        self.assertDictEqual(result, correct_result)
 
     def test_None_is_2nd_arg(self):
         sample_dict1 = {
@@ -193,25 +205,32 @@ class TestExtend(unittest.TestCase):
         sample_dict2_copy = {"number2": 1, "string2": "x", "new_string": "xxx"}
         correct_result = {"number1": 5, "number2": 1, "string1": "peter",
                           "string2": "x", "new_string": "xxx"}
-        extend(sample_dict1, None, sample_dict2)
-        self.assertDictEqual(sample_dict1, correct_result)
+        result = extend(sample_dict1, None, sample_dict2)
+
+        self.assertDictEqual(sample_dict1, result)
+        self.assertDictEqual(result, correct_result)
         self.assertDictEqual(sample_dict2, sample_dict2_copy)
 
     def test_merge_dict_with_Nones(self):
+        # copied from jquery unittest
         options = {"number1": 1, "x_string": "x", "string": "new_string"}
         null_dict = {"number1": None}
         correct_result = {"number1": None, "x_string": "x",
                           "string": "new_string"}
         result = extend(options, null_dict)
+
+        self.assertDictEqual(options, result)
         self.assertEqual(null_dict["number1"], None)
         self.assertDictEqual(result, correct_result)
 
     def test_true_merge_with_list_in_dict(self):
-        dict_1 = {"apple": [{"price": 100, "wieght": 200}, "extra", "extra"]}
+        dict_1 = {"apple": [{"price": 100, "weight": 200}, "extra", "extra"]}
         dict_2 = {"apple": [{"price": 200}, "two", "three"]}
+
         result = extend(True, dict_1, dict_2)
-        correct_result = {"apple": [{"price": 200, "wieght": 200}, "two",
+        correct_result = {"apple": [{"price": 200, "weight": 200}, "two",
                                     "three"]}
+
         self.assertDictEqual(result, correct_result)
 
     def test_true_merge_with_lists_blank_first(self):
@@ -220,6 +239,7 @@ class TestExtend(unittest.TestCase):
         result = extend(True, {}, dict_1, dict_2)
         correct_result = {"apple": [{"price": 200, "weight": 200}, "two",
                                     "three"]}
+        
         self.assertDictEqual(result, correct_result)
 
 if __name__ == '__main__':
